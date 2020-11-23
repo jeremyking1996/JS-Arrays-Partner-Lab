e39a2e3d82c8a14a6e771ded9c8ca280371e51ff
deloris19904 <73267872+deloris19904@users.noreply.github.com>
2020-11-23 14:54:39 -0500

    added comma space between array names

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 let myArray = ["Jeremy","Adam","Deloris"];
 
 
-// for (i = 0; i < myArray.length; i++){
-//     myArray="default";
-//     console.log(myArray);
+ // for (i = 0; i < myArray.length; i++){
+ //    myArray[i]="default";
+ //    console.log( myArray[i]);
+ // }
+
+let names= ""
+for (i =0; i < myArray.length; i++){
+    names+= myArray[i]
+   if (i<myArray.length-1){
+    names+=", "
+   }
+}
+console.log (names)
+
+
+
+
+
+
+
+
+// if (myArray.length === 0){
+//     console.log("array is empty")
+// } else {
+//     console.log("array is full")
 // }
 
 
-
-if (myArray.length === 0){
-    console.log("array is empty")
-} else {
-    console.log("array is full")
-}
+// console.log(myArray.length === 0);
 
 
-console.log(myArray.length === 0);
 
 
+//Given an array `myArray`, display the elements of `myArray` comma delimited.
 
 console.log(myArray);
