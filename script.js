b92711cf6ed2ef1ca1034090360288b0f1bb4245
jeremyking1996 <73255406+jeremyking1996@users.noreply.github.com>
2020-11-23 13:44:56 -0500

    Step 1

---
+++ script.js
@@ -0,0 +1,22 @@
+let myArray = ["Jeremy","Adam","Deloris"];
+
+
+// for (i = 0; i < myArray.length; i++){
+//     myArray="default";
+//     console.log(myArray);
+// }
+
+
+
+if (myArray.length === 0){
+    console.log("array is empty")
+} else {
+    console.log("array is full")
+}
+
+
+console.log(myArray.length === 0);
+
+
+
+console.log(myArray);
